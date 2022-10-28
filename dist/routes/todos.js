d93f0bb2028666bf9f76c6f30086a394725c2f34
Zeno Driesen <zeno.driesen@gmail.com>
2022-10-28 17:00:16 +0200

    controllers, parsing requests, crud

--- dist/routes/todos.js
+++ dist/routes/todos.js
@@ -1,13 +1,14 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = require("express");
+const todos_1 = require("../controllers/todos");
 const router = (0, express_1.Router)();
 // POST TO DO
-router.post("/");
+router.post("/", todos_1.createTodo);
 // GET TO DO
-router.get("/");
+router.get("/", todos_1.getTodos);
 // UPDATE A TO DO
-router.patch("/:id");
+router.patch("/:id", todos_1.updateTodo);
 // DELETE A TO DO
-router.delete("/:id");
+router.delete("/:id", todos_1.deleteTodo);
 exports.default = router;
--- src/routes/todos.ts
+++ src/routes/todos.ts
@@ -1,17 +1,23 @@
 import { Router } from "express";
+import {
+  createTodo,
+  deleteTodo,
+  getTodos,
+  updateTodo,
+} from "../controllers/todos";
 
 const router = Router();
 
 // POST TO DO
-router.post("/");
+router.post("/", createTodo);
 
 // GET TO DO
-router.get("/");
+router.get("/", getTodos);
 
 // UPDATE A TO DO
-router.patch("/:id");
+router.patch("/:id", updateTodo);
 
 // DELETE A TO DO
-router.delete("/:id");
+router.delete("/:id", deleteTodo);
 
 export default router;
